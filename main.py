fa5e7afc17e010ebae568e54491a6e73576fb4b7
Tony Fleming <mtony75@gmail.com>
2018-03-03 16:17:21 -0500

    Added main execution section and functions

--- main.py
+++ main.py
@@ -3,3 +3,42 @@
 # from a user then randomly select the same 
 # choices and choose who wins.
 
+def validChoice(choice):
+	if (int(choice) < 1) | (int(choice) > 3):
+		return False
+	else:
+		return True
+
+def playAgain(game):
+	if (int(game) == 1):
+		return True
+	else:
+		return False
+
+#import random library
+import random
+print('Lets Play \'Rock\' \'Paper\' \'Scissors\'')
+
+# run controls the loop for game play. 1= go 0 = stop
+run = 1
+x = 5
+while (run != 0):
+	print('Your selections')
+	print('1. Rock')
+	print('2. Paper')
+	print('3. Scissors')
+	usrChoice = input('Enter your choice: ')
+	if validChoice(usrChoice):
+		print('Choice is valid')
+	else:
+		print('Choice not valid')
+	print('Play another game?')
+	print('1. Yes')
+	print('2. No')
+	nextGame = input('Choice: ')
+	if playAgain(nextGame):
+		run = 1
+	else:
+		run = 0
+		
+
